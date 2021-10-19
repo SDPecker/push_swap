9d727d595c9cc5a63f25f893a91113922874f410
SDPecker <bulexey@mail.ru>
2021-10-19 13:36:53 +0300

    done bit sort??

--- bit_sort.c
+++ bit_sort.c
@@ -11,10 +11,52 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h> //TESTING ONLY!
 
 void	bit_sort(stack *a, stack *b)
 {
 	int		i;
+	int		j;
+	int		init_size;
 
+	i = 0;
+	j = 0;
+	init_size = a->size;
 	*a = simplify(a);
+	while (i <= get_max_bit(max(*a)))
+	{
+		while (j < init_size)
+		{
+			if (a->array[0] >> i & 1 == 0)
+				exec_pb(a, b);
+			else
+				exec_ra(a);
+			j++;
+		}
+		while (b->size > 0)
+			exec_pa(a, b);
+		j = 0;
+		i++;
+	}
+}
+
+int	get_max_bit(int num)
+{
+	int	tmp;
+	int	res;
+
+	tmp = num;
+	res = 0;
+	while(tmp / 2 > 0)
+	{
+		res++;
+		tmp /= 2;
+	}
+	return(res);
+}
+
+int main(void)
+{
+	printf("%i\n", get_max_bit(138));
+	return (0);
 }
